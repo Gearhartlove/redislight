817600ae01a3c6e6fa9942cae3ae1b87a45fac73
Gearhartlove <finley@kristoff.codes>
2022-10-21 11:44:28 -0600

    Second Commit. Questioning current scope and approach . . . thinking of dialing back.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,28 @@
-mod tokenizer;
 mod commands;
+mod scanner;
 
 use std::io::{stdin, stdout, Write};
-use crate::tokenizer::{RedisTokens, tokenize};
+use crate::scanner::{TokenKind, Scanner, Token};
 
 /// Reads the user input from the command line and tokenizes that input.
-pub fn read() -> Vec<RedisTokens> {
+pub fn read() -> Vec<RedisToken> {
     let mut line = String::new();
 
     print!(">> ");
     let _ = stdout().flush(); // Flushes the input buffer. Used for formatting.
     stdin().read_line(&mut line).unwrap();
 
-    let tokens = tokenize(line);
+    let mut scanner = Scanner::from(&line);
+    let mut tokens: Vec<Token> = Vec::default();
+
+    loop {
+        let token = scanner.scan_token();
+        scanner.advance();
+
+        tokens.push(token);
+    }
+
+
     return tokens;
 }
 
@@ -20,30 +30,30 @@ pub fn parse() {
 
 }
 
-pub fn eval(tokens: Vec<RedisTokens>) -> String {
+pub fn eval(tokens: Vec<RedisToken>) -> String {
     for token in tokens.iter() {
         match token {
-            RedisTokens::CONSTANT(_) => {}
-            RedisTokens::SET => {}
-            RedisTokens::GET => {}
-            RedisTokens::DEL => {}
-            RedisTokens::LPUSH => {}
-            RedisTokens::LPOP => {}
-            RedisTokens::LRANGE => {}
-            RedisTokens::HSET => {}
-            RedisTokens::HGET => {}
-            RedisTokens::EX => {}
-            RedisTokens::PX => {}
-            RedisTokens::EXAT => {}
-            RedisTokens::PXAT => {}
-            RedisTokens::NX => {}
-            RedisTokens::XX => {}
-            RedisTokens::KEEPTTL => {}
+            RedisToken::LITERAL(_) => {}
+            RedisToken::SET => {}
+            RedisToken::GET => {}
+            RedisToken::DEL => {}
+            RedisToken::LPUSH => {}
+            RedisToken::LPOP => {}
+            RedisToken::LRANGE => {}
+            RedisToken::HSET => {}
+            RedisToken::HGET => {}
+            RedisToken::EX => {}
+            RedisToken::PX => {}
+            RedisToken::EXAT => {}
+            RedisToken::PXAT => {}
+            RedisToken::NX => {}
+            RedisToken::XX => {}
+            RedisToken::KEEPTTL => {}
         }
     }
     "".to_string()
 }
 
-pub fn print() {
+pub fn print(result: String) {
 
 }
